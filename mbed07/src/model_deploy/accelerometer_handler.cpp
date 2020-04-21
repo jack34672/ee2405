b684d32c852dfad4eef0b07c93b27ab3d595b039
Jack Chen <jack34672@gmail.com>
2020-04-22 00:50:16 +0800

    udpate

--- mbed07/src/model_deploy/accelerometer_handler.cpp
+++ mbed07/src/model_deploy/accelerometer_handler.cpp
@@ -88,6 +88,7 @@ bool ReadAccelerometer(tflite::ErrorReporter* error_reporter, float* input,
     z = ((float)acc16) / 4096.0f;
 
     sample_skip_counter += 1;
+    
   }
 
   // Write samples to our buffer, converting to milli-Gs
